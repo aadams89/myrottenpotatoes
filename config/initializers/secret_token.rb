9367ea85bd4dc896394aa0cac130fac920ffb5d0
saasbook <saasbook@saasbook.(none)>
2014-10-02 10:01:44 -0300

    start of new ch4 walkthrough'
    git status
    git log(ditto)
    git config --globalcore.editor 'vim'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '373869134742b8980c0e56b7551cedcedeb42a22eebdc2aa6bb03db4b64d72481c1215012381d6649ae42ef0ed910ee95c870c61fe197da1fbe57dbb2296458e'
